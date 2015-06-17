50deb77fc05e21c8621bea0f01681e7707f92a74
PKlein <paulo.klein@actia.com.br>
2015-06-17 17:30:21 -0300

    Geração de arquivo em formato JSON para ListRSS.

--- src/main/java/br/com/actia/gson/ListRSSConverter.java
+++ src/main/java/br/com/actia/gson/ListRSSConverter.java
@@ -3,22 +3,17 @@ package br.com.actia.gson;
 import br.com.actia.model.ListRSS;
 import br.com.actia.model.RSS;
 
-public class ListRSSConverter {
-    private final ListRSS listRSS;
-
+public class ListRSSConverter extends AbstractConverter{
     public ListRSSConverter(ListRSS listRSS) {
-        this.listRSS = listRSS;
-    }
-    /**
-     * Convert ListRSS in JSON String
-     * @return String JSON
-     */
-    public String convert() {
-        GsonGenerator<ListRSS> gsonGen = new GsonGenerator<>();
+        super(listRSS);
         
-        gsonGen.addAdapter(RSS.class, new RSSAdapter());
-        
-        return gsonGen.expToJson(listRSS);
+        this.MAIN_DIRECTORY = "rss";
+        this.buildAdapters();
+        this.path = this.CONTENT_FILES_DIRECTORY + this.SEPARATOR_CHAR + this.MAIN_DIRECTORY;
+        this.filePath = this.path + this.SEPARATOR_CHAR + ((ListRSS)this.Entity).getName() + ".json";
     }
     
+    public void buildAdapters(){
+        gsonGen.addAdapter(RSS.class, new RSSAdapter());
+    }
 }
