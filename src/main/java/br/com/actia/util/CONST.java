c0950f515f9e6cc88649a2079c0f8b10e9372f1e
Armani <anderson.armani@actia.com.br>
2015-07-02 09:51:21 -0300

    CONST
    Alterado limites

--- src/main/java/br/com/actia/util/CONST.java
+++ src/main/java/br/com/actia/util/CONST.java
@@ -9,17 +9,17 @@ public final class CONST {
     public static long AUDIO_MAX_LENGTH = 100000; //Maximum audio lenght in bytes
     //#video
     public static long VIDEO_MAX_TIME = 100000; //in milliseconds
-    public static long VIDEO_MAX_LENGTH = 500000; //500 KB
+    public static long VIDEO_MAX_LENGTH = 100000000; //100MB
     //#Banner
     public static long BANNER_MAX_HEIGTH = 130;
     public static long BANNER_MIN_HEIGTH = 100;
     public static long BANNER_MAX_WIDTH = 1200;
     public static long BANNER_MIN_WIDTH = 1000;
-    public static long BANNER_MAX_LENGTH = 100000; //Maximum lenght in bytes
+    public static long BANNER_MAX_LENGTH = 100000; //Maximum lenght in bytes 100KB
     //#Indication Image
     public static long IMAGE_MAX_HEIGTH = 1080;
     public static long IMAGE_MIN_HEIGTH = 360;
     public static long IMAGE_MAX_WIDTH = 1920;
     public static long IMAGE_MIN_WIDTH = 640;
-    public static long IMAGE_MAX_LENGTH = 400000; //Maximum lenght in bytes
+    public static long IMAGE_MAX_LENGTH = 400000; //Maximum lenght in bytes 400KB
 }
